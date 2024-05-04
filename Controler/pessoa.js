6d53e72e68fb1c88c57c195326a35572f0bb62fe
Pedro <izielucas2016@gmail.com>
2024-05-04 12:31:08 -0300

    to fix error

--- Controler/pessoa.js
+++ Controler/pessoa.js
@@ -2,12 +2,26 @@ import { openDb } from '../configDB.js';
 
 export async function createTable() {
     openDb().then(db => {
-        db.exec('CREATE TABLE IF NOT EXISTS Pessoa ( id INTEGER PRIMARY KEY, nome TEXT, idade INTEGER )')
+        db.exec('CREATE TABLE IF NOT EXISTS Pessoa ( id INTEGER PRIMARY KEY, nome TEXT, idade INTEGER )') //Create the SQLite table if don't exist
     })
 };
 
 export async function insert(pessoa) {
     openDb().then(db => {
-        db.run('INSERT INTO Pessoa (nome, idade) VALUES (?,?)', [pessoa.nome, pessoa.idade]);
+        db.run('INSERT INTO Pessoa (nome, idade) VALUES (?,?)', [pessoa.nome, pessoa.idade]); //Create a new pessoa in the database
+    })
+};
+
+export async function update(pessoa) {
+    openDb().then(db => {
+        db.run('UPDATE Pessoa SET nome=?, idade=? WHERE id=?', [pessoa.nome, pessoa.idade, pessoa.id]); //Update a pessoa in the database based in id
+    })
+};
+
+export async function select(pessoa) {
+    return openDb().then(async db => {
+        const res = await db.all('SELECT * FROM Pessoa') //return all people in dataBase
+            ;
+        return res;
     })
 };
--- server.js
+++ server.js
@@ -1,24 +1,68 @@
+import { createTable, insert, update, select } from './Controler/pessoa.js' // import config of SQLite
 
-import { createTable, insert } from './Controler/pessoa.js'
+//MVC -> Padrão de desenvolvimento
+// Sessions ou JWT ->  controle de usuário
+import express from 'express'; //Import express
+const app = express(); //apply express to an variable
+const PORT = 8080 //Port of server
 
-import express from 'express';
-const app = express();
-const PORT = 8080 || 3030;
-
-createTable();
-app.use(express.json());
+createTable(); //Create the SQLite table if don't exist
+app.use(express.urlencoded({ extended: true }))// Valida forms e instruções 404
+app.use(express.json()); //Use json from express
 
 app.get('/', (req, res) => {
-    res.end("Hello World")
-})
+    res.end("Primeiro end point, não há nada aqui!") //First end point, only an return of a phrase
+});
 
-app.post('/pessoa', (req, res) => {
+app.post('/pessoa', (req, res) => { //endpoint to create a new people on table in SQLite
+    // const { id, nome, idade } = req.body
+    // if (!nome || !idade) { //validate if has name and age
+    //     res.json({
+    //         "statusCode": 400,
+    //         "message": "Está faltando dados para a criação do usuário"
+    //     })
+    //     return
+    // }
+
+    // consultar se a pessoa já existe no banco de dados
+
+    //constroi um ob
+    // const pessoa ={
+    //     id: id,
+    //     nome:nome,
+    //     idade: idade
+    // }
+
     console.log(req.body);
-    insert(req.body)
+    insert(pessoas)
+    insert(req.body) //use the body data from the api and insert in db
     res.json({
-        "statusCode": 200
+        "statusCode": 201,
+        "message": "Usuario criado com sucesso!" //return in json
     })
-})
+});
+
+app.put('/pessoa', (req, res) => {
+    if (req.body && !req.body.id) { //Check if the body contain the id
+        res.json({
+            "statusCode": 400,
+            "message": "Você precisa informar o ID"
+        });
+    }
+    else {
+        console.log(req.body);
+        update(req.body) //update the dataBase based in body
+        res.json({
+            "statusCode": 200,
+            "message": "Usuario criado com sucesso!" //return in json
+        })
+    }
+});
+
+app.get('/pessoas', async (req, res) => {
+    let pessoas = await select(); //in an async function await the Select function from pessoa.js works to use
+    res.json(pessoas) //return in json
+});
 
 app.listen(PORT, () => {
     console.log(`Server aberto na porta ${PORT}`)
